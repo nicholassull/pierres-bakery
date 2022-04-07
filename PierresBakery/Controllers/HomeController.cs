7fcd29521dab917865ed6a6a8c8921bff3782e18
Nick Sullivan <nicholassull@gmail.com>
2022-04-07 15:28:36 -0700

    add HomeController.cs

--- PierresBakery/Controllers/HomeController.cs
+++ PierresBakery/Controllers/HomeController.cs
@@ -1,1 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+
+namespace PierresBakery.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
