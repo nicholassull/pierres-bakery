88c3cffb13100c057fac8050b8a3c928a54f0e23
Nick Sullivan <nicholassull@gmail.com>
2022-04-07 15:37:10 -0700

    add Index and Register routes in AccountController

--- PierresBakery/Controllers/AccountController.cs
+++ PierresBakery/Controllers/AccountController.cs
@@ -18,5 +18,28 @@ namespace PierresBakery.Controllers
       _signInManager = signInManager;
       _db = db;
     }
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+    public IActionResult Register()
+    {
+      return View();
+    }
+    [HttpPost]
+    public async Task<ActionResult> Register (RegisterViewModel model)
+    {
+      var user = new ApplicationUser { UserName = model.Email };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
   }
 }
