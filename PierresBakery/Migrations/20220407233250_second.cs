268bb2ae282259e5071839df9d3071c6cb6c8242
Nick Sullivan <nicholassull@gmail.com>
2022-04-07 16:33:32 -0700

    update error in FlavorTreats table with new migration

---
+++ PierresBakery/Migrations/20220407233250_second.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PierresBakery.Migrations
+{
+    public partial class second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FlavorTreats_Treats_TreatId",
+                table: "FlavorTreats");
+
+            migrationBuilder.DropColumn(
+                name: "RecipeId",
+                table: "FlavorTreats");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TreatId",
+                table: "FlavorTreats",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FlavorTreats_Treats_TreatId",
+                table: "FlavorTreats",
+                column: "TreatId",
+                principalTable: "Treats",
+                principalColumn: "TreatId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FlavorTreats_Treats_TreatId",
+                table: "FlavorTreats");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TreatId",
+                table: "FlavorTreats",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RecipeId",
+                table: "FlavorTreats",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FlavorTreats_Treats_TreatId",
+                table: "FlavorTreats",
+                column: "TreatId",
+                principalTable: "Treats",
+                principalColumn: "TreatId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
