24aa74ff5220267ced4ec8a7b518b2304c2f1580
Nick Sullivan <nicholassull@gmail.com>
2022-04-08 16:25:05 -0700

    add Create route for Flavors

---
+++ PierresBakery/Controllers/FlavorsController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PierresBakery.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace PierresBakery.Controllers
+{
+  public class FlavorsController : Controller
+  {
+    private readonly PierresBakeryContext _db;
+    
+    public FlavorsController(PierresBakeryContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Flavor> model = _db.Flavors.OrderBy(flavor => flavor.Name).ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
