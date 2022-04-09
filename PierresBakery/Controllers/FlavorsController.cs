7033d85ffe6192ecaf51da8a829a2d13b45d8d7d
Nick Sullivan <nicholassull@gmail.com>
2022-04-09 13:11:36 -0700

    add Delete view and route for Flavors

--- PierresBakery/Controllers/FlavorsController.cs
+++ PierresBakery/Controllers/FlavorsController.cs
@@ -43,5 +43,19 @@ namespace PierresBakery.Controllers
       .FirstOrDefault(flavor => flavor.FlavorId == id);
       return View(thisFlavor);
     }
+    public ActionResult Delete (int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+      
+    }
+    [HttpPost,ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
